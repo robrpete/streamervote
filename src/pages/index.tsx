b56d0329016ecf5e252aef0becd467012f92f63c
robrpete <rob.r.pete@gmail.com>
2023-09-21 23:55:25 -0400

    edit #1 card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,7 +40,7 @@ export default function Home() {
             <h2 className="pb-4 text-3xl font-bold text-purple-400 mob:text-4xl">
               Streamer
             </h2>
-            <div className="flex h-56 w-fit flex-col items-center rounded-xl px-2">
+            <div className="flex h-56 w-fit flex-col items-center rounded-xl px-2 py-2">
               <Image
                 src="/rawrr.png"
                 height={150}
@@ -51,6 +51,19 @@ export default function Home() {
               <p className="text-xl font-semibold text-purple-400">zackrawrr</p>
               <p>votes: 99,999</p>
               <p className="text-red-600">not live</p>
+              <div className="flex gap-12 text-black">
+                <button className="rounded-lg bg-purple-400 px-2 py-1">
+                  vote
+                </button>
+                <div className="flex gap-2">
+                  <button className="rounded-lg bg-blue-600 px-2 py-1">
+                    up
+                  </button>
+                  <button className="rounded-lg bg-red-600 px-2 py-1">
+                    down
+                  </button>
+                </div>
+              </div>
             </div>
             <div className="flex w-full flex-col items-center pt-16">
               <h2 className="pb-4 text-3xl font-bold text-purple-400 mob:text-4xl">
