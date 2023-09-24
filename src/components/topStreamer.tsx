61de506e62e61f3ea37c8f249b0868bc1a34aee8
robrpete <rob.r.pete@gmail.com>
2023-09-24 14:50:52 -0400

    edit top card

--- src/components/topStreamer.tsx
+++ src/components/topStreamer.tsx
@@ -3,13 +3,8 @@ import Image from "next/image";
 export default function TopStreamer() {
   return (
     <div>
-      <h2 className="pt-4 text-3xl font-bold text-purple-400 mob:pt-2 mob:text-4xl">
-        Number One
-      </h2>
-      <h2 className="pb-4 text-3xl font-bold text-purple-400 mob:text-4xl">
-        Streamer
-      </h2>
-      <div className="flex w-fit flex-col items-center rounded-xl px-2 py-2">
+      <div className="flex h-fit w-56 flex-col items-center px-2 py-2">
+        <p className="pb-4 text-3xl font-semibold">zackrawrr</p>
         <Image
           src="/rawrr.png"
           height={150}
@@ -17,15 +12,11 @@ export default function TopStreamer() {
           alt="ava"
           className="rounded-full border-4"
         />
-        <p className="text-xl font-semibold text-purple-400">zackrawrr</p>
-        <p>votes: 99,999</p>
+
+        <p className="pt-2">votes: 99,999</p>
         <p className="text-red-600">not live</p>
-        <div className="flex gap-12 text-black">
-          <button className="rounded-lg bg-purple-400 px-2 py-1">vote</button>
-          <div className="flex gap-2">
-            <button className="rounded-lg bg-blue-600 px-2 py-1">up</button>
-            <button className="rounded-lg bg-red-600 px-2 py-1">down</button>
-          </div>
+        <div className="flex w-full flex-col gap-2 text-black">
+          <button className=" rounded-lg bg-yellow-400 px-2 py-1">vote</button>
         </div>
       </div>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,11 +37,14 @@ export default function Home() {
           <SidePanel />
           <div className="absolute -z-10 hidden h-40 w-40 scale-[1.5] rounded-full bg-purple-700 blur-3xl transition-opacity duration-300 sm:block"></div>
           <div className="flex w-full flex-col items-center text-center md:pt-2">
+            <h2 className="py-4 text-3xl font-bold mob:pt-2 mob:text-4xl">
+              Top Streamer
+            </h2>
             <div
               ref={cardEl}
               onMouseMove={handleMouseMove}
               onMouseLeave={handleMouseLeave}
-              className="relative z-10 h-fit overflow-hidden"
+              className="relative z-10 h-fit overflow-hidden rounded-lg border-[1px] border-white/20"
             >
               <div
                 ref={circleEl}
@@ -50,7 +53,7 @@ export default function Home() {
                   top: position.top,
                   opacity: opacity,
                 }}
-                className="absolute -z-10 hidden h-20 w-20 scale-[1.2] bg-yellow-400 blur-3xl transition-opacity duration-300 sm:block"
+                className="absolute -z-10 hidden h-20 w-20 scale-[1.2] bg-[#eab308] blur-3xl transition-opacity duration-300 sm:block"
               ></div>
               <TopStreamer />
             </div>
