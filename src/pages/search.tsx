61fd0e984ae8f7e528824190a82b1f689d8b4584
robrpete <rob.r.pete@gmail.com>
2023-10-10 01:28:43 -0400

    border search cards, check data, maybe new endpoint

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -6,23 +6,28 @@ export default function Results() {
   const votes = useStore((state) => state.votes);
   const { searched } = searchedStore();
   const arr = Array.from(searched);
-
+  console.log(arr);
   return (
     <div>
       results {votes}
       <button className="rounded-lg bg-blue-600 p-1">test</button>
       search:
       {arr.map((a) => (
-        <div key={a.r.id}>
-          <Image
-            src={a.r.thumbnail_url}
-            width={40}
-            height={40}
-            alt={a.r.display_name}
-          />
-          <Link href="/" className="hover:text-purple-400">
-            {a.r.display_name}
-          </Link>
+        <div key={a.r.id} className="flex border-2">
+          <div>
+            <Image
+              src={a.r.thumbnail_url}
+              width={40}
+              height={40}
+              alt={a.r.display_name}
+            />
+          </div>
+          <div>
+            <Link href="/" className="hover:text-purple-400">
+              {a.r.display_name}
+            </Link>
+            <p>{a.r.game_name}</p>
+          </div>
         </div>
       ))}
     </div>
